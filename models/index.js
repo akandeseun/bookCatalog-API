8f38c86f0297d43a4a63125364ade26fe040113b
akandeseun <akandeseun44@gmail.com>
2022-08-03 15:27:58 +0100

    defined associations

--- models/index.js
+++ models/index.js
@@ -1,8 +1,7 @@
-'use strict'
-
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
+
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
 const config = require(__dirname + '/../config/config.json')[env]
@@ -39,6 +38,79 @@ Object.keys(db).forEach((modelName) => {
     db[modelName].associate(db)
   }
 })
+
+// Associations
+// db.Author.hasMany(db.Like, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false',
+//   scope: {
+//     likeableType: 'authors'
+//   }
+// })
+// db.Like.belongsTo(db.Author, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false'
+// })
+
+// db.Book.hasMany(db.Like, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false',
+//   scope: {
+//     likeableType: 'books'
+//   }
+// })
+// db.Like.belongsTo(db.Book, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false'
+// })
+
+// db.BookDetail.hasMany(db.Like, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false',
+//   scope: {
+//     likeableType: 'bookdetails'
+//   }
+// })
+// db.Like.belongsTo(db.BookDetail, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false'
+// })
+
+// db.Category.hasMany(db.Like, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false',
+//   scope: {
+//     likeableType: 'categories'
+//   }
+// })
+// db.Like.belongsTo(db.Category, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false'
+// })
+
+// db.Publisher.hasMany(db.Like, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false',
+//   scope: {
+//     likeableType: 'publishers'
+//   }
+// })
+// db.Like.belongsTo(db.Publisher, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false'
+// })
+
+// db.User.hasMany(db.Like, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false',
+//   scope: {
+//     likeableType: 'users'
+//   }
+// })
+// db.Like.belongsTo(db.User, {
+//   foreignKey: 'likeableId',
+//   constraints: 'false'
+// })
 
 db.sequelize = sequelize
 db.Sequelize = Sequelize
